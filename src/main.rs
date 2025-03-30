c282f29c168402baf0222c659a9a66aad962b7fc
Chris <chrishall0801@gmail.com>
2025-03-30 16:35:58 -0400

    Added button to add more video players

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod video_pipeline;
-use gtk::{gdk::Display, glib, prelude::*, Application, ApplicationWindow, Box, Builder, CssProvider, Window};
+use gtk::{gdk::Display, glib, prelude::*, Application, ApplicationWindow, Box, Builder, CssProvider, Button};
 use gstgtk4;
 mod widgets;
 use widgets::video_player_widget::video_player::VideoPlayer;
@@ -20,28 +20,51 @@ fn load_css() {
     }
 }
 
-fn build_ui(app: &Application) {
+fn build_ui(app: &Application) -> Builder {
     let builder = Builder::from_resource("/mainwindow/mwindow.ui");
     load_css();
 
+    // let button: Button = builder.object("new_video_player_button").expect("Failed to get button from UI file");
+
+    // let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window from UI file");
+    // let video_container: Box = builder.object("video_container").expect("Failed to get video_container from UI File");
+    // button.connect_clicked(glib::clone!(
+    //     #[weak(rename_to = window_clone)] window,
+    //     #[weak(rename_to = video_container_clone)] video_container,
+    //     move |_| {
+    //         let new_player = VideoPlayer::new();
+    //         new_player.setup_event_handlers(window_clone);
+    //         video_container_clone.append(&new_player);
+    //     }
+    // ));
+    
+    
+    
     let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window from UI file");
 
-    let video_container: Box = builder.object("video_container").expect("failed to get video_container from UI file");
+    //let video_container: Box = builder.object("video_container").expect("failed to get video_container from UI file");
 
-    let player1 = VideoPlayer::new();
-    let player2 = VideoPlayer::new();
-    video_container.append(&player1);
-    video_container.append(&player2);
+    // let player1 = VideoPlayer::new();
+    // let player2 = VideoPlayer::new();
+    // let player3 = VideoPlayer::new();
+    // video_container.append(&player1);
+    // video_container.append(&player2);
+    // video_container.append(&player3);
     
-    let window_clone = window.clone();
-    player1.setup_event_handlers(window_clone);
+    // let window_clone = window.clone();
+    // player1.setup_event_handlers(window_clone);
 
-    let window_clone = window.clone();
-    player2.setup_event_handlers(window_clone);
+    // let window_clone = window.clone();
+    // player2.setup_event_handlers(window_clone);
+
+    // let window_clone = window.clone();
+    // player3.setup_event_handlers(window_clone);
 
     app.add_window(&window);
     window.show();
+    builder
 }
+
 
 fn main() -> glib::ExitCode {
     gstreamer::init().unwrap();
@@ -59,28 +82,8 @@ fn main() -> glib::ExitCode {
     
     let app = gtk::Application::new(None::<&str>, gtk::gio::ApplicationFlags::FLAGS_NONE);
     app.connect_activate(|app| {
-
-        // let window = gtk::ApplicationWindow::new(app);
-        
-        // window.set_default_size(640, 480);
-        // window.set_title(Some("Video Player"));
-
-        // let player1 = VideoPlayer::new();
-        // let player2 = VideoPlayer::new();
-
-        // let container = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-        // container.append(&player1);
-        // container.append(&player2);
-        
-        // window.set_child(Some(&container));
-
-        // player1.setup_event_handlers();
-        // player2.setup_event_handlers();
-
-        // app.add_window(&window);
-
-        // window.show();
-        build_ui(app);
+        let builder = build_ui(app);
+        set_up_button(builder);
     });
 
     let res = app.run();
@@ -89,4 +92,16 @@ fn main() -> glib::ExitCode {
         gstreamer::deinit();
     }
     res
+}
+
+fn set_up_button(builder: Builder) {
+    let button: Button = builder.object("new_video_player_button").expect("Failed to get button");
+    
+    button.connect_clicked(move |_| {
+        let video_container: Box = builder.object("video_container").expect("failed to get video_container from UI file");
+        let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window from UI file");
+        let new_player = VideoPlayer::new();
+        new_player.setup_event_handlers(window);
+        video_container.append(&new_player);
+    });
 }
